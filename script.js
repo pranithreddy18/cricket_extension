15e5f6e831341ec14733217ee343ec6230d1dbd7
Devireddy Pranith Reddy <141638191+pranithreddy18@users.noreply.github.com>
2023-08-15 20:03:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=a02a65ea-31f4-4ebf-ad2d-6e045bc9c9bf&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
